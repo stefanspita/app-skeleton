14fe2cb2020f41a71bdc35860ee2014d1d3b216d
Stefano <stefan.spita@bluespeckfinancial.co.uk>
2016-11-26 11:31:13 +0000

    Fix the rollup setup with eslint and got the project up to date with the tutorial

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,34 @@
 // Rollup plugins
-import babel from "rollup-plugin-babel"
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
-import uglify from "rollup-plugin-uglify"
+import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: "src/scripts/main.js",
-  dest: "build/js/main.min.js",
-  format: "iife",
-  sourceMap: "inline",
+  entry: 'src/scripts/main.js',
+  dest: 'build/js/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
   plugins: [
-    babel({
-      exclude: "node_modules/**",
-    }),
     resolve({
       jsnext: true,
       main: true,
       browser: true,
     }),
     commonjs(),
-    uglify(),
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
   ],
-}
+};
