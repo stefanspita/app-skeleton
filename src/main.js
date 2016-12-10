1ac9edf8ee138dc6ff8b0492e36cf4e3593846e7
Stefano <stefan.spita@bluespeckfinancial.co.uk>
2016-12-10 17:27:12 +0000

    Add the redux logger as middleware to the app (aids debugging)

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import {Provider} from "react-redux"
 import {createStore} from "redux"
 import {reducers} from "./app"
 import App from "./app"
+import getStoreMiddleware from "./app/store-middleware"
 
-let store = createStore(reducers)
+let store = createStore(reducers, getStoreMiddleware())
 
 ReactDOM.render(
   <Provider store={store}>
