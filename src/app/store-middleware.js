8c874ab25b695596eb7ecccd0c10fbc6c34d719b
Stefan <stefan.spita@bluespeckfinancial.co.uk>
2017-05-22 16:31:26 +0100

    Update packages to latest and fix the build

--- src/app/store-middleware.js
+++
@@ -1,8 +0,0 @@
-import createLogger from "redux-logger"
-import {applyMiddleware} from "redux"
-
-export default function getStoreMiddleware() {
-  return applyMiddleware(
-    createLogger({collapsed: true})
-  )
-}
